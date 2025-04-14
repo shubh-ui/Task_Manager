f4447af61c18a0f8411f485d78c033a9020a9b53
Shubham <shubhampatil@metamorphtech.com>
2025-04-15 00:48:50 +0530

    fixed issues in protect middleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,24 +1,25 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const envConstants = require("../constants/envConstants");
 
 
 // Middleware to protect route
 
 const protect = async (req, res, next) => {
     try {
-        const token = req.header.authorization;
+        let token = req.headers.authorization;
 
         if (token && token.startsWith("Bearer")) {
             token = token.split(" ")[1]; //Extract token;
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const decoded = jwt.verify(token, envConstants.JWT_SECRET);
             req.user = await User.findById(decoded.id).select("-password");
             next()
         }
         else {
-            res.status(401).message({ message: "Not authorized, No token" });
+            res.status(401).json({ message: "Not authorized, No token" });
         }
     } catch (error) {
-        res.status(401).message({ message: "Token failed", error: error.message })
+        res.status(401).json({ message: "Token failed", error: error.message })
     }
 }
 
