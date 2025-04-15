e012cf253d1d79a30d3beeefdd08023dd7b97b4c
Shubham <shubhampatil@metamorphtech.com>
2025-04-16 01:06:22 +0530

    added getuserbyid route handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,7 +38,10 @@ const getUsers = async (req, res) => {
 
 const getUserById = async (req, res) => {
     try {
+        const user = await User.findById(req.params.id).select("-password");
 
+        if(!user) return res.status(404).json({message: "user not found"});
+        res.status(200).json(user);
     } catch (error) {
         res.status(500).json({ message: "Internal server error." })
     }
