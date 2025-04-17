3e074fd8a765cb8107ec28774457949916c8f754
Shubham <shubhampatil@metamorphtech.com>
2025-04-17 18:59:32 +0530

    added new route handller to addmember to the project

--- backend/controllers/projectContoller.js
+++ backend/controllers/projectContoller.js
@@ -22,5 +22,38 @@ const createProject = async (req, res) => {
 };
 
 
+const addMembersToProject = async (req, res) => {
+    const projectId = req.params.projectId;
+    const memberIds  = req.body.members;
 
-module.exports = { createProject }
+    try {
+        const project = await Project.findById(projectId);
+
+        if(!project) return res.status(404).json({message: " Project not found."});
+
+        if(project.createdBy.toString() != req.user._id) {
+            return res.status(403).json({message: "Only the project creator can add members"})
+        }
+
+        if(!Array.isArray(memberIds)) {
+            return res.status(400).json({message: "memberIds must be an array"});
+        }
+
+        const newMembers = memberIds.filter(memberId => !project.members.includes(memberId));
+
+        if(newMembers.length) {
+            project.members.push(...newMembers);
+            await project.save();
+        }
+
+        res.status(201).json({message: "members added sucessfully.", newMembers});
+
+
+    } catch (error) {
+        res.status(500).json({message : "Internal server error."});
+    }
+}
+
+
+
+module.exports = { createProject , addMembersToProject }
--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { protect, adminOnly } = require("../middlewares/authMiddleware");
-const { createProject } = require("../controllers/projectContoller");
+const { createProject, addMembersToProject } = require("../controllers/projectContoller");
 
 const router = express.Router();
 
 router.post("/", protect, adminOnly, createProject);
+router.post("/:projectId/members", protect, adminOnly, addMembersToProject);
 
 module.exports = router;
