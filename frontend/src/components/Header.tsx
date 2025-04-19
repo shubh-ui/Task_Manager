c93a49a8d9c4c35f8ae97573ce98df97afb20545
Shubham <shubhampatil@metamorphtech.com>
2025-04-19 17:42:01 +0530

    Added link tag to buttons and styled the logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { Button } from "./ui/button"
 
 const Header = () => {
@@ -5,12 +6,12 @@ const Header = () => {
         <div>
             <div className="fixed top-0 left-0 right-0 z-50 bg-white shadow-md h-16 flex justify-between items-center px-6">
                 <div>
-                    <h3 className="text-3xl font-extrabold">TaskZen <span>.</span></h3>
+                    <h3 className="text-3xl font-extrabold">TaskZen <span className="text-blue-500">.</span></h3>
                 </div>
                 <div>
                     <div className="flex gap-4">
-                        <Button className="bg-blue-500 text-white hover:bg-blue-600">Sign up</Button>
-                        <Button variant={"secondary"}>Request Demo</Button>
+                        <Link to={"/login"}><Button className="bg-blue-500 text-white hover:bg-blue-600 cursor-pointer">Sign up</Button></Link>
+                        <Button className="cursor-pointer" variant={"secondary"}>Request Demo</Button>
                     </div>
                 </div>
             </div>
