77bad5cc09ac083a992e8d1ce954f282be75f1c2
Shubham Patil <shubhpatil8510@gmail.com>
2025-04-19 18:23:48 +0530

    added api paths

---
+++ frontend/src/utils/api-path.ts
@@ -0,0 +1,24 @@
+export const BASE_URL = "http://localhost:5000";
+
+export const API_PATH = {
+  AUTH : {
+    REGISTER: "/api/auth/register",
+    LOGIN: "/api/auth/login",
+    GET_PROFILE: "api/auth/profile"
+  },
+  USERS : {
+    GET_ALL_USERS: "",
+    GET_USER_BY_ID: "",
+    CREATE_USER: "",
+    UPDATE_USER: ""
+  },
+  PROJECTS : {
+
+  },
+  TASKS : {
+
+  },
+  IMAGE : {
+    UPLOAD_IMAGE : "api/auth/upload-image"
+  }
+}
